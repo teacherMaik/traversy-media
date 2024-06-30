51b15155d9ebb0768240dd79cbfa957a027fc763
teacherMaik <d.michaelthomasbennett@gmail.com>
2024-06-30 23:27:41 +0200

    late night commit

--- react-crash-course/src/components/JobsListings.jsx
+++ react-crash-course/src/components/JobsListings.jsx
@@ -1,9 +1,31 @@
+import { useState, useEffect } from 'react';
 import JobListing from './JobListing';
-import jobs from '../jobs.json';
+import { FaLongArrowAltUp } from 'react-icons/fa';
 
 const JobsListings = ({ isHome = false }) => {
 
-  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    const fetchJobs = async () => {
+
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+        setJobs(data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+  
+    };
+
+    fetchJobs();
+  }, []);
+  /* const jobListings = isHome ? jobs.slice(0, 3) : jobs; */
 
   return (
 
@@ -13,11 +35,7 @@ const JobsListings = ({ isHome = false }) => {
           {isHome ? 'Recent Jobs' : 'All Jobs'}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {jobListings.map((job) => (
-
-            <JobListing key = {job.id} job = {job}/>
-          ))};
-
+          {jobs.map((job) => (<JobListing key = {job.id} job = {job}/>))};
         </div>
       </div>
     </section>
